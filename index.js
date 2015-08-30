6fbb1b28728c3301a6310af9dd9c1628b2bc20ae
bsmckamey <brenden.mckamey@gmail.com>
2015-08-30 16:37:12 -0500

    screensonsite

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var express = require('express');
+var express = require('express')
+    , sendEmail = require('./routes/sendEmail.js');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,8 +14,28 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/quote', function(request, response) {
+  response.render('pages/quote');
+});
+
+app.get('/submitQuote', function(request, response) {
+    var firstName = request.query.firstName
+    var lastName = request.query.lastName
+    var streetAddress = request.query.streetAddress
+    var city = request.query.city
+    var state = request.query.state
+    var zipCode = request.query.zipCode
+    var daytimePhone = request.query.daytimePhone
+    var eveningPhone = request.query.eveningPhone
+    var emailAddress = request.query.emailAddress
+    var comments = request.query.comments
+    
+    sendEmail.sendQuote(firstName, lastName, streetAddress, city, state, zipCode, daytimePhone, eveningPhone, emailAddress, comments, function(statusCode, result) {
+        console.log("Email sent...");
+    })
+    response.render('pages/quote');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
---
+++ routes/sendEmail.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require("nodemailer");
+
+exports.sendQuote = function(firstName, lastName, streetAddress, city, state, zipCode, daytimePhone, eveningPhone, emailAddress, comments, onResult) {
+    // create reusable transporter object using SMTP transport
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'brenden.mckamey@gmail.com',
+            pass: '#c4v3eXpl0r3r'
+        }
+    });
+
+    // NB! No need to recreate the transporter object. You can use
+    // the same transporter object for all e-mails
+
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: 'Brenden McKamey <brenden.mckamey@gmail.com>', // sender address
+        to: 'brenden.mckamey@gmail.com', // list of receivers
+        subject: 'ScreensOnsite Quote Information', // Subject line
+        text: 'First Name: ' + firstName + '\n' + 'Last Name: ' + lastName + '\n' + 'Street Address: ' + streetAddress + '\n' + 'City: ' + city + '\n' + 'State: ' + state + '\n' + 'Zip Code: ' + zipCode + '\n' + 'Daytime Phone: ' + daytimePhone + '\n' + 'Evening Phone: ' + eveningPhone + '\n' + 'Email Address: ' + emailAddress + '\n' + 'Comments: ' + comments, // plaintext body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+
+    });
+};
