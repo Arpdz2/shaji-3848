607a1e3c91d0a5592b34565efd08f1aaff9db585
arpdz2 <arpdz2@umsl.edu>
2015-09-26 18:05:57 -0500

    added session functionality

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({ secret: 'qwdqwdqwdqwdrfergwefdwcwcqcqwdlkqwjmqwdi' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
