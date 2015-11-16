b0cf735bd9ad21b0bf203041b4bbc479cac05cb4
arpdz2 <arpdz2@umsl.edu>
2015-11-15 21:20:41 -0600

    test

--- index.js
+++ index.js
@@ -46,11 +46,10 @@ var pseudoArray = ['admin'];
 var users = 0; //count the users
 
 io.sockets.on('connection', function (socket) { // First connection
-    var socketId = socket.id;
-    //client ip
-    var clientIp = socket.request.connection.remoteAddress;
-    console.log(clientIp);
-
+    var clientIpAddress= socket.request.socket.remoteAddress;
+    console.log("**************");
+    console.log(clientIpAddress);
+    console.log("**************");
     users += 1; // Add 1 to the count
     reloadUsers(); // Send the count to all the users
     socket.on('message', function (data) { // Broadcast the message to all
